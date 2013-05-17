2a1249a4e34a037ce9996da8d37b6c9dd9692478
bart.straetmans <bartje.straetmans@fake.be>
2013-05-17 14:10:59 +0200

    add solution

---
+++ pajocoons/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pajocoons.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Pajocoons Cattery";
+
+            return View();
+        }
+
+        public ActionResult Males()
+        {
+            return View();
+        }
+        public ActionResult Females()
+        {
+            return View();
+        }
+        public ActionResult Kittens()
+        {
+            return View();
+        }
+        public ActionResult Others()
+        {
+            return View();
+        }
+        public ActionResult Media()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ pajocoons/Models/AccountModels.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace pajocoons.Models
+{
+    public class RegisterExternalLoginModel
+    {
+
+        [Display(Name = "Gebruikersnaam")]
+        public string UserName { get; set; }
+
+
+        [Display(Name = "E-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        public string ExternalLoginData { get; set; }
+    }
+
+    public class LocalPasswordModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Huidig Wachtwoord")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Het {0} moet minstenst {2} karakters lang zijn.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nieuw Wachtwoord")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Bevestig Wachtwoord")]
+        [Compare("NewPassword", ErrorMessage = "De wachtwoorden komen niet overeen met elkaar.")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "E-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+    }
+
+    public class LoginModel
+    {
+
+        [Display(Name = "Gebruikersnaam")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "E-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Wachtwoord")]
+        public string Password { get; set; }
+
+        [Display(Name = "Onthoud me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterModel
+    {
+
+        [Required]
+        [Display(Name = "Gebruikersnaam")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "E-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Het {0} moet minstens {2} karakters lang zijn.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Wachtwoord")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Bevestig Wachtwoord")]
+        [Compare("Password", ErrorMessage = "De wachtwoorden komen niet overeen met elkaar.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ExternalLogin
+    {
+        public string Provider { get; set; }
+        public string ProviderDisplayName { get; set; }
+        public string ProviderUserId { get; set; }
+    }
+}
